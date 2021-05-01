4269a200a3b4809f9255b875dc1a8c3c1dba82f5
j_kuszewski <jkkuszewski@gmail.com>
2021-05-01 23:24:20 +0200

    Add Graph class and tests

---
+++ Models/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Graph<T>
+    {
+        protected LinkedList<T> adjacencyList;
+        public Graph()
+        {
+            adjacencyList = new LinkedList<T>();
+        }
+    }
+}
---
+++ Tests/GraphTests.cs
@@ -0,0 +1,15 @@
+using Models;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class GraphTests
+    {
+        [Fact]
+        public void AfterAddingEdgesReturnsProperAdjacencyList()
+        {
+            Graph<string> graph = new Graph<string>();
+        }
+    }
+}
