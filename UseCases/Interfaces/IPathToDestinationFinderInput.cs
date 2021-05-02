9440e7213b5d5fccec94bc09e765aa9ec130d127
j_kuszewski <jkkuszewski@gmail.com>
2021-05-03 01:45:13 +0200

    Add PathToDestinationFinder class and tests for it

---
+++ UseCases/Interfaces/IPathToDestinationFinderInput.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCases.Interfaces
+{
+    public interface IPathToDestinationFinderInput
+    {
+        public Map GetMap();
+    }
+}
---
+++ UseCases/Interfaces/IPathToDestinationFinderOutput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCases.Interfaces
+{
+    public interface IPathToDestinationFinderOutput
+    {
+        public List<int> GetVisitedVerticesList();
+    }
+}
