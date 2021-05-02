e818e5b14f7bca3a51fb2b856fba699ca4fe46ba
j_kuszewski <jkkuszewski@gmail.com>
2021-05-02 17:07:39 +0200

    Add Map and MapTests classes

---
+++ Models/Map.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Map
+    {
+        private readonly Dictionary<string, int> territoriesDictionary;
+        private readonly UndirectedGraph graphRepresentation;
+        public Map(string[] territories)
+        {
+            territoriesDictionary = new Dictionary<string, int>();
+            for(int i = 0; i < territories.Length; i++)
+                territoriesDictionary.Add(territories[i], i);
+
+            graphRepresentation = new UndirectedGraph(territories.Length);
+        }
+
+        public List<int>[] GetAdjacencyList()
+        {
+            return graphRepresentation.GetAdjacencyList();
+        }
+
+        public void AddBorder(string from, string to)
+        {
+            graphRepresentation.AddEdge(territoriesDictionary[from],
+                                        territoriesDictionary[to]);
+        }
+    }
+}
---
+++ Tests/MapTests.cs
@@ -0,0 +1,35 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class MapTests
+    {
+        [Fact]
+        public void AfterAddingTerritoriesAndCommonBordersReturnsProperGraphAdjacencyList()
+        {
+            string[] territoryCodes = new string[]
+            {
+                "CAN",
+                "USA",
+                "MEX"
+            };
+            Map map = new Map(territoryCodes);
+            map.AddBorder("CAN", "USA");
+            map.AddBorder("USA", "MEX");
+
+
+            Assert.Equal<List<int>>(
+                    new List<int>[3] {
+                        new List<int> { 1 },
+                        new List<int> { 0, 2 },
+                        new List<int> { 1 }
+                    },
+                    map.GetAdjacencyList()
+                );
+        }
+    }
+}
