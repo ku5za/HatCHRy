a1c85077436e4040fa5cb3ad0780a524e8d5ece7
j_kuszewski <jkkuszewski@gmail.com>
2021-05-03 00:25:28 +0200

    Add MapBuilder class

---
+++ UseCases/MapBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UseCases.Interfaces;
+
+namespace UseCases
+{
+    public class MapBuilder : IMapBuilderOutput
+    {
+        private readonly Map itsMap;
+        public MapBuilder(IMapBuilderInput mapBuilderInput)
+        {
+            itsMap = new Map(mapBuilderInput.GetVerticesCodes());
+            List<string>[] bordersList = mapBuilderInput.GetBordersList();
+            foreach(var borderPair in bordersList)
+            {
+                itsMap.AddBorder(borderPair[0], borderPair[1]);
+            }
+        }
+
+        public Map GetMap()
+        {
+            return itsMap;
+        }
+    }
+}
