f6a8ab8e674d5d69e17f68dfdbe08ef3d3f79542
j_kuszewski <jkkuszewski@gmail.com>
2021-05-03 00:24:28 +0200

    Add MapBuider class interfaces

---
+++ UseCases/Interfaces/IMapBuilderInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace UseCases.Interfaces
+{
+    public interface IMapBuilderInput
+    {
+        public string[] GetVerticesCodes();
+        public List<string>[] GetBordersList();
+    }
+}
