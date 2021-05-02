dbe219f7ee2d021f58f8e64cc470a587bd00ef4e
j_kuszewski <jkkuszewski@gmail.com>
2021-05-03 00:25:17 +0200

    Add tests for MapBuilder class

---
+++ Tests/UseCasesTests/MapBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.UseCasesTests.Mocks;
+using UseCases;
+using Xunit;
+
+namespace Tests.UseCasesTests
+{
+    public class MapBuilderTests
+    {
+        [Fact]
+        public void GetAdjacencyList_SimpleMockMapBuilderInput_ReturnsMapWithProperAdjacencyList()
+        {
+            MockMapBuilderInput mockMapBuilder = new MockMapBuilderInput();
+            MapBuilder mapBuilder = new MapBuilder(mockMapBuilder);
+            Map map = mapBuilder.GetMap();
+            Assert.Equal(
+                    map.GetAdjacencyList(),
+                    new List<int>[]
+                    {
+                        new List<int> { 1 },
+                        new List<int> { 0, 2 },
+                        new List<int> { 1, 3 },
+                        new List<int> { 2 },
+                    }
+                );
+        }
+    }
+}
---
+++ Tests/UseCasesTests/Mocks/MockMapBuilderInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UseCases;
+using UseCases.Interfaces;
+
+namespace Tests.UseCasesTests.Mocks
+{
+    public class MockMapBuilderInput : IMapBuilderInput
+    {
+        public List<string>[] GetBordersList()
+        {
+            return new List<string>[]
+            {
+                new List<string> { "CAN", "USA" },
+                new List<string> { "USA", "MEX" },
+                new List<string> { "MEX", "BLZ" }
+            };
+        }
+
+        public string[] GetVerticesCodes()
+        {
+            return new string[] { "CAN", "USA", "MEX", "BLZ" };
+        }
+    }
+}
