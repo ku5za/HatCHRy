b98f3ff4427d679abe5de62cd3aedd981072efcd
j_kuszewski <jkkuszewski@gmail.com>
2021-05-03 16:07:44 +0200

    Add MockShortestPathFinderInput class for tests

---
+++ Tests/UseCasesTests/Mocks/MockShortestPathFinderInput.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UseCases.Interfaces;
+
+namespace Tests.UseCasesTests.Mocks
+{
+    public class MockShortestPathFinderInput : IShortestPathFinderInput
+    {
+        public Map GetMap()
+        {
+            Map map = new Map(new string[] { "CAN", "USA", "MEX", "BLZ", "GTM", "SLV", "HND" });
+            map.AddBorder("CAN", "USA");
+            map.AddBorder("USA", "MEX");
+            map.AddBorder("MEX", "BLZ");
+            map.AddBorder("MEX", "GTM");
+            map.AddBorder("BLZ", "GTM");
+            map.AddBorder("GTM", "SLV");
+            map.AddBorder("GTM", "HND");
+            return map;
+        }
+    }
+}
