4763686f3132f04d5327985f8094d8cbfc9ae458
j_kuszewski <jkkuszewski@gmail.com>
2021-05-04 02:23:51 +0200

    Add Web API GetPathFromUsaTo endpoint

---
+++ Controllers/Controllers/GetPathFromUsaTo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UseCases.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetPathFromUsaTo : ControllerBase
+    {
+        private readonly IShortestPathFinderOutput pathFinderOutput;
+        public GetPathFromUsaTo(IShortestPathFinderOutput shortestPathFinderOutput)
+        {
+            pathFinderOutput = shortestPathFinderOutput;
+        }
+
+        // GET api/<GetPathFromUsaTo>/BLZ
+        [HttpGet("{code}")]
+        public string[] Get(string code)
+        {
+            return pathFinderOutput.GetVisitedVerticesList("USA", code).ToArray();
+        }
+    }
+}
